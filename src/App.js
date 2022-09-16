c107db7c9e05c5d90424cc5b2242ca13a9553fa1
chrisgonzalez0896 <chrisgonzalez0896@gmail.com>
2022-09-15 19:56:58 -0500

    progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Measure from './components/Measure';
@@ -7,11 +7,27 @@ import MeasureTab from './components/MeasureTab';
 
 function App() {
 
+  const measureRefsArr = [useRef()];
+  const measures = [];
+  
+  const display = measureRefsArr.map(measure => {
+    let currentMeasure = <MeasureTab measureRef={measure}/>;
+    measures.push(currentMeasure);
+    return currentMeasure;
+  });
+
+  console.log(display)
+
+  function handle(){
+    for(let measureRef of measures){
+      console.log(measureRef)
+    }
+  }
+
   return (
-    <>
-      <MeasureTab />
-      <Measure />
-    </>
+    <div onClick={handle}>
+      {display}
+    </div>
   );
 }
 
--- src/components/MeasureTab.js
+++ src/components/MeasureTab.js
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, {useRef} from 'react';
+
+// const test = useRef();
 
 class MeasureTab extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
           selected: false,
+          strings: [{ note: 'e', selected: false} ,{ note: 'B', selected: false}, { note: 'G', selected: false},
+                    { note: 'D', selected: false} ,{ note: 'A', selected: false}, { note: 'E', selected: false}],
           timeSignature: [ 4, 4 ],
-          timeLeft: 480
+          timeLeft: 2310
         };
       this.handleClick = this.handleClick.bind(this);
+      this.display = <div className={this.state.selected ? 'selected' : null} onClick={this.handleClick}>
+        {this.state.strings.map((string, index) => {
+        return <div className={index === 0 ? 'string top' : 'string'}>{string.note}</div>
+      })}</div>
+    }
+
+    controls(e){
+      console.log('here: ', e.key);
+    }
+    
+    keyboardController(selected) {
+      selected ? window.addEventListener('keydown', this.controls, false)
+      : window.removeEventListener('keydown', this.controls, false);
+      
     }
   
     handleClick() {
+      const currentStrings = this.state.strings;
+      console.log('this is measuretab')
       this.setState(prevState => ({
         selected: !prevState.selected
       }));
+      this.display = <div className={this.state.selected ? 'selected' : null} onClick={this.handleClick}>
+      {this.state.strings.map((string, index) => {
+      return <div className={index === 0 ? 'string top' : 'string'}>{string.note}</div>
+    })}</div>
+    this.keyboardController(this.state.selected)
+    if(!this.state.selected) window.removeEventListener('keydown', ()=>{console.log('removed')}, false)
     }
   
     render() {
       return (
-        <button onClick={this.handleClick}>
-          {this.state.selected ? 'Selected' : 'Not'}
-        </button>
+
+        this.display
+        // <button onClick={this.handleClick}>
+        //   {this.state.selected ? 'Selected' : 'Not'}
+        // </button>
       );
     }
   }
